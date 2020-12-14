2e2c98b31b21d152c0ab5772fb528796565550d1
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-12-14 23:36:58 +0300

    Renamed licence

--- example/workspace.jl
+++ example/workspace.jl
@@ -1,15 +1,13 @@
+# Simple file to simulation discrete time chaotic systems.
+
 using DifferentialEquations 
 using Plots 
 
 function f(dx, x, u, t) 
-    return dx[1] = -x[1] 
+    dx[1] = -x[1] 
 end
 
 x0 = ones(1) 
 tspan = (0., 10.) 
 prob = ODEProblem(f, x0, tspan) 
 sol = solve(prob) 
-
-
-
-
