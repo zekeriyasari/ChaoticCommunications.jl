d11a7c585ce4dc8934a51892da45d22e3245f8e1
zekeriyasari <zekeriya.sari.deu@gmail.com>
2022-05-12 09:49:47 +0300

    Bug fix in theoretical ber calculation.

--- example/monte_carlo_csk_lorenz.jl
+++ example/monte_carlo_csk_lorenz.jl
@@ -1,5 +1,4 @@
 using ChaoticCommunications
-using SpecialFunctions
 using PyPlot
 pygui(true)
 close("all")
@@ -7,7 +6,7 @@ close("all")
 # Construct communication system 
 k = 1
 M = 2^k
-ns = Int(1e4)
+ns = Int(5e4)
 tsymbol = 10.0
 tsample = 0.01
 comsys = CommunicationSystem(
@@ -18,12 +17,29 @@ comsys = CommunicationSystem(
 )
 
 # Run simulation 
-esno = 0:2:8
-symerr = ber_numerical(comsys, esno)
+esno = 0:2:12
+
+# Transmisssion
+msg = comsys.symbolgen.symbols
+refs, tx = comsys.modulator(msg)
+
+# Corruption and detection 
+symerr_numerical = map(esno) do val
+    @info "Running $val dB"
+    comsys.channel.esno = val
+    rx = comsys.channel(tx)
+    msgext = comsys.detector(refs, rx)
+    sum(msg .!= msgext) / length(msg)
+end
+
+nusers = 1
+symerr_theoretical = map(val -> bermacsk(val, comsys.modulator.gens[1], tsymbol, tsample, nusers), esno)
 
 # Plots 
-plot(esno, symerr, marker="o")
+plot(esno, symerr_numerical, marker="o")
+plot(esno, symerr_theoretical, marker="*")
 yscale("log")
 grid(which="both", axis="both")
 xlabel("EsNo [dB]")
 ylabel("Pe")
+ylim(10^-5, 10^0)
--- src/theoretical.jl
+++ src/theoretical.jl
@@ -2,15 +2,25 @@
 export bermacsk
 
 """
-    $SIGNATURES 
-
+    $TYPEDSIGNATURES 
+        
 Return probability error for `ebno` for a given `cmap`. `numsamples` is the number of samples per symbol and `numusers` is
 the number of users.
 """
-function bermacsk(ebno, cmap, numsamples, numusers)
-    x2 = trajectory!(cmap, numsamples - 1).^2
+function bermacsk(ebno::Real, cmap::AbstractDiscreteOscillator, numsamples::Int, numusers::Int)
+    x2 = trajectory!(cmap, numsamples) .^ 2
     Ω = var(x2) / (mean(x2)^2)
     1 / 2 * erfc(
-        (2 * Ω / numsamples + 2 * (2 * numusers - 1)/numsamples + 2 * (dbtoval(ebno)^(-1)))^(-1 / 2)
-        )
+        (2 * Ω / numsamples + 2 * (2 * numusers - 1) / numsamples + 2 * (dbtoval(ebno)^(-1)))^(-1 / 2)
+    )
 end
+
+function bermacsk(ebno::Real, cmap::AbstractContinuousOscillator, tsymbol::Real, tsample::Real, numusers::Int)
+    nsamples = floor(Int, tsymbol / tsample)
+    x2 = trajectory!(cmap, tsymbol, tsample) .^ 2
+    Ω = var(x2) / (mean(x2)^2)
+    1 / 2 * erfc(
+        (2 * Ω / nsamples + 2 * (2 * numusers - 1) / nsamples + 2 * (dbtoval(ebno)^(-1)))^(-1 / 2)
+    )
+end
+
